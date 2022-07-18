794b7a4cbcaf6b7124075c97e85119b2223aeb66
Fabien Lecouve <fabien.lecouve@hotmail.com>
2022-07-18 12:37:55 +0200

    Deuxième commit: les fixtures avec faker ont été créé avec succès sur les entités category et product

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     *
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        for ( $i=1; $i<=10; $i++ )
+        {
+            $category = new Category();
+            $category->setName($this->faker->word());
+            $manager->persist($category);
+            for ( $j=1; $j<=10; $j++ )
+            {
+                $product = new Product();
+                $product->setName($this->faker->word())
+                    ->setUnitPrice($this->faker->randomNumber(3, false))
+                    ->setDescription($this->faker->sentence(10))
+                    ->setDisponibility($this->faker->numberBetween(0, 1))
+                    ->setCategory($category);
+                $manager->persist($product);
+            }
+        }
 
         $manager->flush();
     }
